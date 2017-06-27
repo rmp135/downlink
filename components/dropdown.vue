2895447bbe8be0a8df9c3b26b0f666ac01419a64
Ryan Poole <rmp135@gmail.com>
2017-06-27 23:49:09 +0100

    Dropdown component.
    The built in select component can't be styled in the right way. This is a replacement that  works in almost the same way.

---
+++ components/dropdown.vue
@@ -0,0 +1,62 @@
+<style lang="scss">
+  .dropdown {
+    position: relative;
+    &:hover {
+      .options {
+        display: block;
+      }
+    }
+    .options {
+      display: none;
+      box-sizing: border-box;
+      position: absolute;
+      width: 100%;
+      background: black;
+      border: 1px green solid;
+      padding: 5px;
+      .option:hover {
+        cursor: pointer;
+        background-color: green;
+        color: black;
+      }
+    }
+    .picked {
+      align-items: center;
+      display: flex;
+      height: 1rem;
+      border: 1px green solid;
+      &:hover {
+        background-color: green;
+        color: black;
+      }
+    }
+  }
+</style>
+<template lang="pug">
+  .dropdown
+    .picked {{picked}}
+    .options
+      .option(v-for="option in options", @click="onSelect(option)") {{option}}
+</template>
+<script>
+  export default {
+    props: {
+      options: {
+        type: Array,
+        required: true
+      }
+    },
+    data: () => ({
+      picked: ''
+    }),
+    mounted () {
+      this.picked = this.options[0]
+    },
+    methods: {
+      onSelect (picked) {
+        this.picked = picked
+        this.$emit('input', this.picked)
+      }
+    }
+  }
+</script>
